3831d46c57f5589f8dd174eac8df8065e8f5a6d5
elianiva <dicha.arkana03@gmail.com>
2021-01-24 15:13:05 +0700

    feat: improved proxy route

--- src/routes/jobs.json.ts
+++ src/routes/jobs.json.ts
@@ -1,11 +1,26 @@
 import fetch from "node-fetch"
 
 export const get = async (req: any) => {
-  const raw = await fetch(
-    `https://jobs.github.com/${req.query.get(
-      "kind"
-    )}.json?search=${req.query.get("keyword")}&page=${req.query.get("page")}`
-  )
+  const { query: q } = req
+
+  const filterObj = {
+    search: q.get("search") || "",
+    page: q.get("page") || "",
+    location: q.get("location") || "",
+    full_time: q.get("full_time") ?? false,
+  }
+
+  const filter = Object.entries(filterObj)
+    .map(([key, val]) => (val !== "" ? `${key}=${val}` : ""))
+    .join("&")
+    .replace(/&&/g, "&") // this seems hacky, it's used to remove double `&&` when the provided query is an empty string
+
+  const url =
+    q.get("kind") === "all"
+      ? `https://jobs.github.com/positions.json?${filter}`
+      : `https://jobs.github.com/positions/${q.get("id")}.json`
+
+  const raw = await fetch(url)
   const result = await raw.json()
 
   return {
