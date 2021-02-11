0e226a1562ddb2dfa833cf880a4f1298c1571fe3
elianiva <dicha.arkana03@gmail.com>
2021-02-11 17:22:10 +0700

    feat: SEO component

---
+++ src/components/SEO.svelte
@@ -0,0 +1,11 @@
+<svelte:head>
+  <title>{title} | devjobs</title>
+  <link rel="canonical" href="https://devjobs.vercel.app/{path}" />
+  <meta name="description" content="some random description" />
+  <meta name="keywords" content="dev jobs github" />
+</svelte:head>
+
+<script lang="ts">
+export let title: string
+export let path: string
+</script>
