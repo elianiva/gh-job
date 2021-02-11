01df737b932087c0117552e079b1f64e5d7a3ebe
elianiva <dicha.arkana03@gmail.com>
2021-02-11 17:22:49 +0700

    feat: implement load more button

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -15,13 +15,37 @@
   text-align: center;
   color: #b0b0b0;
 }
+
+.button {
+  padding: 1rem 2rem;
+  border: none;
+  margin: 0 auto;
+  color: #ffffff;
+  background-color: #5964e0;
+  display: block;
+  font-family: "Poppins", sans-serif;
+  font-weight: 600;
+  font-size: 1rem;
+  cursor: pointer;
+  border-radius: 0.25rem;
+}
+
+.loading-text {
+  display: block;
+  text-align: center;
+  font-family: "Poppins", sans-serif;
+  font-weight: 400;
+  font-size: 1.25rem;
+  color: #555555;
+}
 </style>
 
+<SEO title="Home" path=""/>
 <Search />
 {#if $jobs.length < 1}
   <div class="loading">
     {#if $isFound}
-      <LoadingBar />
+      <Loading />
     {:else}
       <h1 class="error">Not found, Please try with another keyword!</h1>
     {/if}
@@ -40,6 +64,13 @@
       />
     {/each}
   </div>
+  {#if isFetching}
+    <p class="loading-text">Loading more data...</p>
+  {:else}
+    <button class="button" on:click={fetchNextPage}>
+      See More
+    </button>
+  {/if}
 {/if}
 
 <script context="module" lang="ts">
@@ -54,9 +85,23 @@ export const load = async ({ fetch }) => {
 </script>
 
 <script lang="ts">
+import SEO from "#components/SEO.svelte"
 import Search from "#components/SearchBar.svelte"
 import Card from "#components/Card.svelte"
-import LoadingBar from "#components/Loading.svelte"
-
+import Loading from "#components/Loading.svelte"
 import { isFound } from "#stores/found_status"
+import { filter } from "#stores/filter"
+
+let isFetching = false
+
+$: console.log($filter)
+
+const fetchNextPage = async () => {
+  isFetching = true
+  const req = await fetch("/jobs.json?kind=all&page=2")
+  const res = await req.json()
+
+  jobs.set([...$jobs, ...res])
+  isFetching = false
+}
 </script>
